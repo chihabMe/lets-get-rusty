5a72e1ee1bd8d8bcc023460c00e5fb4b12db5c91
chihabMg <chihab.mg.me@gmail.com>
2023-06-09 17:46:25 +0100

    finished the compression functinality

--- compressor/src/main.rs
+++ compressor/src/main.rs
@@ -1,30 +1,35 @@
-extern  crate flate2;
-use flate2::write::GzEncoder;
-use flate2::Compression;
-use std::env::args;
-use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
+use std::error::Error;
+use std::io::{BufReader, copy};
 use std::time::Instant;
+use flate2::{write::GzEncoder, Compression};
+use std::fs::File;
+use std::env::args;
 
+fn compress_file() -> Result<(), Box<dyn Error>> {
+    let mut output_file_name = args().nth(2).ok_or("invalid file name").unwrap();
+    if !output_file_name.contains(".gz"){
+        output_file_name+=".gz"
+    }
+    let file = File::open(args().nth(1).ok_or("No input file provided")?)?;
+    let mut file_buffer = BufReader::new(file);
+
+    let output = File::create(output_file_name)?;
+    let start = Instant::now();
+
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    copy(&mut file_buffer, &mut encoder)?;
+
+    let result = encoder.finish()?;
+    println!("source len: {:?}", file_buffer.get_ref().metadata()?.len());
+    println!("compressed len: {:?}", result.metadata()?.len());
+    println!("time:{:?}", start.elapsed());
+
+    Ok(())
+}
 
 fn main() {
-    if args().len()<3{
-        println!("-------------------------\n");
-        println!("Usage: `file path` `name` \n");
-        eprintln!("Example: ./mgcomp hello.py hello \n");
-        println!("-------------------------");
-        return ;
+    if let Err(error) = compress_file() {
+        eprintln!("Error: {}", error);
+        std::process::exit(1);
     }
-    let filename = args().nth(1).unwrap_or_else(||{
-        eprintln!("a file path is required");
-        std::process::exit(1);
-    });
-    println!("{}",filename);
-    let file = File::open(filename).unwrap_or_else(|error|{
-        println!("{}",error);
-        std::process::exit(1);
-    });
-    println!("{:?}",file);
-    // // let mut file_buffer = BufReader::new(inner)
 }
